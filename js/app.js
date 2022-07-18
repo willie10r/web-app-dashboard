53be85546b99c43078f8a01a5deaf0ccdeebb685
willie m <zx10rwillie@gmail.com>
2022-07-17 19:02:07 -0500

    updating js

--- js/app.js
+++ js/app.js
@@ -1,2 +1,15 @@
 const alertBanner = document.getElementById('alert')
-alertBanner.innerHTML = 'you have new messages';
+alertBanner.innerHTML = '<div class="alert-banner"><p><strong>Alert:</strong> You have <strong>6</strong> overdue tasksto complete</p><p class="alert-banner-close">x</p></div>';
+
+console.log(alertBanner);
+
+
+
+alertBanner.addEventListener("click", e => {
+    const element = e.target;
+  if(element.classList.contains("alert-banner-close")) {
+    alertBanner.style.display = "none"
+  };
+} );
+
+
