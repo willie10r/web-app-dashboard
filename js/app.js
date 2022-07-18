cebe3748b9c2c65ba639bdb2d03681dd88095ce7
willie m <zx10rwillie@gmail.com>
2022-07-18 18:53:01 -0500

    Updating JS
    Trying to get the graphs set up and working properly.

--- js/app.js
+++ js/app.js
@@ -1,16 +1,6 @@
 const alertBanner = document.getElementById('alert');
+const trafficCanvas = document.getElementById("traffic-chart");
 
-
-alertBanner.innerHTML = '<div class="alert-banner"><p><strong>Alert:</strong> You have <strong>6</strong> overdue tasksto complete</p><p class="alert-banner-close">x</p></div>';
-
-console.log(alertBanner);
-
-alertBanner.addEventListener("click", e => {
-    const element = e.target;
-  if(element.classList.contains("alert-banner-close")) {
-    alertBanner.style.display = "none"
-  }
-} );
 let trafficData = {
   labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3",
   "4-10", "11-17", "18-24", "25-31"],
@@ -20,5 +10,67 @@ let trafficData = {
   backgroundColor: 'rgba(116, 119, 191, .3)',
   borderWidth: 1,
   }]
-  };
+};
+let trafficOptions = {
+    backgroundColor: 'rgba(112, 104, 201, .5)',
+    fill: true,
+    aspectRatio: 2.5,
+    animation: {
+      duration: 0
+    },
+    scales: {
+      y: {
+      beginAtZero: true
+      }
+    },
+    plugins: {
+      legend: {
+      display: false
+      }
+    } 
+};
+let trafficChart = new Chart(trafficCanvas, {
+  type: 'line',
+  data: trafficData,
+  options: trafficOptions
+  }); 
+const dailyCanvas = document.getElementById("daily-chart");
+const dailyData = {
+    labels: ["S", "M", "T", "W", "T", "F", "S"],
+    datasets: [{
+    label: '# of Hits',
+    data: [75, 115, 175, 125, 225, 200, 100],
+    backgroundColor: '#7477BF',
+    borderWidth: 1
+    }]
+};
+const dailyOptions = {
+  scales: {
+    y: {
+    beginAtZero: true
+    }
+  },
+  plugins: {
+    legend: {
+    display: false
+    }
+  }
+};
+let dailyChart = new Chart(dailyCanvas, {
+  type: 'bar',
+  data: dailyData,
+  options: dailyOptions
+});
+alertBanner.innerHTML = '<div class="alert-banner"><p><strong>Alert:</strong> You have <strong>6</strong> overdue tasksto complete</p><p class="alert-banner-close">x</p></div>';
+
+console.log(alertBanner);
+
+alertBanner.addEventListener("click", e => {
+    const element = e.target;
+  if(element.classList.contains("alert-banner-close")) {
+    alertBanner.style.display = "none";
+  }
+} );
+
+
 
