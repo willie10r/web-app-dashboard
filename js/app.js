19616b7aa5471fd65e0013fe11f4a27e8c2acb41
willie m <zx10rwillie@gmail.com>
2022-07-17 19:46:54 -0500

    updating js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 const alertBanner = document.getElementById('alert')
+
+
 alertBanner.innerHTML = '<div class="alert-banner"><p><strong>Alert:</strong> You have <strong>6</strong> overdue tasksto complete</p><p class="alert-banner-close">x</p></div>';
 
 console.log(alertBanner);
-
-
 
 alertBanner.addEventListener("click", e => {
     const element = e.target;
@@ -11,5 +11,14 @@ alertBanner.addEventListener("click", e => {
     alertBanner.style.display = "none"
   };
 } );
-
+let trafficData = {
+  labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3",
+  "4-10", "11-17", "18-24", "25-31"],
+  datasets: [{
+  data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500,
+  2500],
+  backgroundColor: 'rgba(116, 119, 191, .3)',
+  borderWidth: 1,
+  }]
+  };
 
