68c339238018a2a53b03de1ae66b24c21a03716d
willie m <zx10rwillie@gmail.com>
2022-07-20 19:09:15 -0500

    updating html and js
    finished up the js and fixing the html a little still need to get the css in order

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 const alertBanner = document.getElementById('alert');
+const user = document.getElementById("userField");
+const message = document.getElementById("messageField");
+const send = document.getElementById("send");
+
 alertBanner.innerHTML = '<div class="alert-banner"><p><strong>Alert:</strong> You have <strong>6</strong> overdue tasksto complete</p><p class="alert-banner-close">x</p></div>';
-
 console.log(alertBanner);
-
 alertBanner.addEventListener("click", e => {
     const element = e.target;
   if(element.classList.contains("alert-banner-close")) {
@@ -10,5 +12,15 @@ alertBanner.addEventListener("click", e => {
   }
 } );
 
-
+send.addEventListener('click', e => {
+  if (user.value === "" && message.value === "") {
+    alert("Please fill out user and message fields before sending");
+    } else if (user.value === "" ) {
+    alert("Please fill out user field before sending");
+    } else if (message.value === "" ) {
+    alert("Please fill out message field before sending");
+    } else {
+    alert(`Message successfully sent to: ${user.value}`);
+    }
+});
 
